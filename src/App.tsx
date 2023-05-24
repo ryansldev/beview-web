0d0bf90e39cea67ebbba2fa62dc82966269c4ba2
ryansldev <ryanlima1310@gmail.com>
2023-05-24 12:45:46 -0300

    :rocket: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-[#000] text-[#fff] h-screen">
+      <h1 className="text-2xl">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
